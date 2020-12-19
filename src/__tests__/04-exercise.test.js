0e3dfcb0a5f42b8c3f4a1ec2740fe6fa26befc51
Dani Lucaci <danilucaci@gmail.com>
2020-12-19 14:47:17 +0100

    Add 04-exercise solution

--- src/__tests__/04-exercise.test.js
+++ src/__tests__/04-exercise.test.js
@@ -32,6 +32,11 @@ describe("04-exercises", () => {
      */
 
     // Write the assertions
+    const user = createUserObject("Alex", "Spence", "alex@mail.com");
+
+    expect(user.firstName).toBeTruthy();
+    expect(user.lastName).toBeTruthy();
+    expect(user.email).toBeTruthy();
   });
 
   test("createCorporateEmailAddress appends the corporate email domain", () => {
@@ -45,6 +50,7 @@ describe("04-exercises", () => {
      */
 
     // Write the assertion
+    expect(createCorporateEmailAddress("dani")).toMatch(/@company.com/);
   });
 
   test("generateRandomNumberFrom1to10 returns a number between 1 and 10", () => {
@@ -56,5 +62,7 @@ describe("04-exercises", () => {
      */
 
     // Write the assertions
+    expect(generateRandomNumberFrom1to10()).toBeGreaterThan(0);
+    expect(generateRandomNumberFrom1to10()).toBeLessThan(11);
   });
 });
