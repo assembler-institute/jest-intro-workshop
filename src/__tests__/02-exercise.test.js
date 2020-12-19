555fdf1a64d283898298d9c253a3a99ad5932883
Dani Lucaci <danilucaci@gmail.com>
2020-12-19 12:17:04 +0100

    Add 02-exercise solution

--- src/__tests__/02-exercise.test.js
+++ src/__tests__/02-exercise.test.js
@@ -8,6 +8,7 @@ describe("02-exercises", () => {
     expect.assertions(1);
 
     // Finish the test
+    expect(hello).toBe("hello-world");
   });
   test("the variable `hello` is not the same as 'hola-mundo'", () => {
     const hello = "hello-world";
@@ -15,6 +16,7 @@ describe("02-exercises", () => {
     expect.assertions(1);
 
     // Finish the test
+    expect(hello).not.toBe("hola-mundo");
   });
   test("user1 is the same as user2", () => {
     expect.assertions(1);
@@ -23,6 +25,7 @@ describe("02-exercises", () => {
     const user2 = user1;
 
     // Finish the test
+    expect(user1).toBe(user2);
   });
   test("user1 is not the same as user3", () => {
     expect.assertions(1);
@@ -31,5 +34,6 @@ describe("02-exercises", () => {
     const user3 = { name: "ana", phone: 678876567 };
 
     // Finish the test
+    expect(user1).not.toBe(user3);
   });
 });
