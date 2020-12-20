29da91848cf079f35c3b081c60a6558b3a687c96
Dani Lucaci <danilucaci@gmail.com>
2020-12-20 16:02:59 +0100

    Add 10-exercise solution

--- src/__tests__/10-exercise.test.js
+++ src/__tests__/10-exercise.test.js
@@ -7,6 +7,7 @@ describe("10-exercises", () => {
   beforeAll(() => {
     // 1. set the default inner html of the document body
     //    to have a div with a class of .wrapper
+    document.body.innerHTML = `<div class='wrapper'></div>`;
   });
 
   test("adds a button to the dom", () => {
@@ -17,12 +18,15 @@ describe("10-exercises", () => {
 
     // 2. execute the addButton function and pass it as an argument
     //    the `btnText` variable
+    addButton(btnText);
 
     // 3. use document.querySelector to get the new button
     //    the `addButton` creates a button with a class of .btn
     //    that you can use in document.querySelector
+    const button = document.querySelector(".btn");
 
     // 4. make an assertion that the buttons textContent is
     //    the same as the `btnText` variable
+    expect(button.textContent).toBe(btnText);
   });
 });
