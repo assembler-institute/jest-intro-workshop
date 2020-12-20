3763f51533d41587b0a61a36f33f7dc135a085af
Dani Lucaci <danilucaci@gmail.com>
2020-12-20 11:26:24 +0100

    Add 08-exercise solution

--- src/__tests__/08-exercise.test.js
+++ src/__tests__/08-exercise.test.js
@@ -9,6 +9,7 @@ describe("08-exercises", () => {
   beforeAll(() => {
     // 1. Spy on the 'add' method of the calculator object
     //    and save the spy in the `addSpy` variable
+    addSpy = jest.spyOn(calculator, "add");
   });
 
   afterAll(() => addSpy.mockRestore());
@@ -17,13 +18,16 @@ describe("08-exercises", () => {
     /**
      * 2. Execute the `increment` by passing the arguments: (4, 5, calculator)
      */
+    increment(4, 5, calculator);
 
     // You should make 2 assertions in this test, see bellow
     expect.assertions(2);
 
     // 3. Make an assertion that the addSpy has been called 1 time
+    expect(addSpy).toBeCalledTimes(1);
 
     // 4. Make an assertion that the addSpy has been called with the
     // numbers 4 and 5
+    expect(addSpy).toHaveBeenCalledWith(4, 5);
   });
 });
